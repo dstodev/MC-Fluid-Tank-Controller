cec83ef161f3052077501a9af066dcfecf35ad91
dstodev <dstodev@gmail.com>
2017-09-24 15:01:30 -0700

    .

--- lib/configurator.lua
+++ lib/configurator.lua
@@ -30,11 +30,10 @@ gpu.fill(1, 1, max_x, max_y, " ")
 create_button("one", 2, 2, 6, 6, function () print("Ayy") end)
 
 while ev.pull(1, "interrupted") == nil do
+    _, x, y, a = ev.pull(0, "touch")
+    print(_, x, y, a)
     for k, v in pairs(buttons) do
-        print(k)
-        for k2, v2 in pairs(v) do
-            print(k2, v2)
-        end
+        
     end
 end
 
