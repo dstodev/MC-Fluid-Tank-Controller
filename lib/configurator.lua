4957bd64faf3d3835e20af6b542066bd4970bbbb
dstodev <dstodev@gmail.com>
2017-09-24 14:22:07 -0700

    Touch test 2

--- lib/configurator.lua
+++ lib/configurator.lua
@@ -12,6 +12,7 @@ local max_x, max_y = gpu.maxResolution()
 -- Functions
 local function create_button(id, x1, y1, x2, y2, callback)
     gpu.fill(x1, y1, x2 - x1, y2 - y1, "█")
+    buttons.id = id
     buttons.id.x1 = x1
     buttons.id.y1 = y1
     buttons.id.x2 = x2
@@ -25,10 +26,13 @@ gpu.setResolution(max_x, max_y)
 -- Clear the screen
 gpu.fill(1, 1, max_x, max_y, " ")
 
+-- Create test screen
+create_button("one", 2, 2, 6, 6, function () print("Ayy") end)
+
 while ev.pull(1, "interrupted") == nil do
     for k, v in pairs(buttons) do
 
     end
 end
 
-create_button("one", 2, 2, 6, 6, function () print("Ayy") end)
+
